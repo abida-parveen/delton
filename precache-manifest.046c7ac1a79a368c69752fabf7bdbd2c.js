4a1475c0397c4e6d568a1ca4b2713205131347df
Abida Parveen <abida.parveen840@gmail.com>
2020-07-06 17:49:20 +0500

    Updates

---
+++ precache-manifest.046c7ac1a79a368c69752fabf7bdbd2c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "990d57116e488ff083980429d20c19b6",
+    "url": "/delton/index.html"
+  },
+  {
+    "revision": "4ae42d916428a03b8929",
+    "url": "/delton/static/css/main.6a8df083.chunk.css"
+  },
+  {
+    "revision": "019c4f71ececcd8198a9",
+    "url": "/delton/static/js/2.21782694.chunk.js"
+  },
+  {
+    "revision": "a1ba88b2875a6244fb7c28fd39d7dc60",
+    "url": "/delton/static/js/2.21782694.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ae42d916428a03b8929",
+    "url": "/delton/static/js/main.24a2362f.chunk.js"
+  },
+  {
+    "revision": "662189c4ced215476514",
+    "url": "/delton/static/js/runtime-main.6d533a4c.js"
+  },
+  {
+    "revision": "19ef7b17bc9414c63c7781251ce0d24d",
+    "url": "/delton/static/media/crown.19ef7b17.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/delton/static/media/shopping-bag.30f07071.svg"
+  }
+]);
